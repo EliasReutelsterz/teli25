3009f2cc80b26f31c4b73543a0c813a9c52394ea
TiborCornelli <tibor.cornelli@t-online.de>
2025-04-06 17:55:41 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,10 @@
+### Commands
+
+# Run python script on a screen window:
+screen -S flasky
+python app.py
+
+# Show & kill running processes
+ps aux | grep python
+kill [1234]
+
--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def seite2():
     return render_template('seite2.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
